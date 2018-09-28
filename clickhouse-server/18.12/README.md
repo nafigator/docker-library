dfb0f1b7fea11e0316286343a278d0a574ed005f
Yancharuk Alexander <alex@itvault.info>
2018-09-28 19:15:24 +0300

    Update clickhouse-server image

--- clickhouse-server/18.12/README.md
+++ clickhouse-server/18.12/README.md
@@ -10,15 +10,34 @@ Suggested path for `docker-compose.yml` root of your project.
 ```yaml
 version: '3.6'
 services:
-  clickhouse-server:
-    image: nafigat0r/clickhouse-server:18.12
-    networks:
-      - network
-    ports:
-      - 9888:9888
+    clickhouse-server:
+      image: nafigat0r/clickhouse-server:18.12
+      environment:
+        DATABASE_NAME: db_name
+        DATABASE_PORT: 9888
+      networks:
+        - network
+      ports:
+        - 9888:9888
+      # init sql-files and custom config
+      volumes:
+        - ./clickhouse-server/18.12/config.xml:/etc/clickhouse-server/config.xml
+        - ./clickhouse-server/18.12/initdb.d:/docker-entrypoint-initdb.d
 networks:
   network:
     driver: bridge
+```
+Put your sql-files to `initdb.d` dir.  Available formats:
+`*.sql`
+`*.sql.gz`
+`*.csv`
+`*.csv.gz`
+
+For `docker logs` usage change clickhouse logs to:
+
+```xml
+<log>/proc/self/fd/2</log>
+<errorlog>/proc/self/fd/2</errorlog>
 ```
 
 ### Build and run container
---
+++ clickhouse-server/18.12/docker-entrypoint.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+# usage: process_init_file FILENAME CLICKHOUSE_COMMAND...
+#    ie: process_init_file foo.sql clickhouse client
+process_init_file() {
+	local file="$1"; shift
+
+	case "$file" in
+		# create databases, tables
+		*.sql)    echo "$0: running $file"
+				  clickhouse client "$(port)" "$(database)" --query="$(cat "$file")"
+				  echo;;
+		*.sql.gz) echo "$0: running $file"
+		          clickhouse client "$(port)" "$(database)" --query="$(gunzip -c "$file")"
+				  echo;;
+		# insert data
+		*.csv)    echo "$0: running $file"
+		          cat "$file" | clickhouse client "$(port)" "$(database)" --query="INSERT INTO test FORMAT CSV"
+		          echo;;
+		*.csv.gz) echo "$0: running $file"
+		          gunzip -c "$file" | clickhouse client "$(port)" "$(database)" --query="INSERT INTO test FORMAT CSV"
+		          echo;;
+		*)        echo "$0: ignoring $file";;
+	esac
+	echo
+}
+
+# get database params
+database() {
+	if [ ! -z ${DATABASE_NAME} ]; then
+		echo "--database=$DATABASE_NAME"
+	fi
+}
+
+# get clickhouse-server port
+port() {
+	if [ ! -z ${DATABASE_PORT} ]; then
+		echo "--port=$DATABASE_PORT"
+	fi
+}
+
+main() {
+    if [ "$1" = "/usr/bin/clickhouse-server" ]; then
+        if [ ! -z ${DATABASE_NAME} ]; then
+            "$@" &
+            local readonly pid="$!"
+            local result="$?"
+
+            for i in {30..0}; do
+                echo "DEBUG: \$i: $i"
+                clickhouse client "$(port)" --query="SHOW DATABASES" &> /dev/null
+                result="$?"
+
+                if [ ${result} = "0" ]; then
+                    break
+                fi
+
+                sleep 1
+            done
+
+            if [ "$i" = 0 ]; then
+                echo >&2 'Clickhouse init process failed.'
+                exit 1
+            fi
+
+            clickhouse client "$(port)" --query="CREATE DATABASE IF NOT EXISTS $DATABASE_NAME"
+
+            for file in /docker-entrypoint-initdb.d/*; do
+                process_init_file "$file"
+            done
+
+            if ! kill -s TERM "$pid" || ! wait "$pid"; then
+                echo >&2 'Clickhouse init process failed.'
+                exit 1
+            fi
+
+            export DATABASE_NAME=
+        fi
+    fi
+
+    exec "$@"
+}
+
+main "$@"
