7c55b8e2f24229e63d2036501a47a70760b45e9a
Yancharuk Alexander <alex@itvault.info>
2021-07-10 13:46:37 +0300

    feat: Add composer 2.1.3 build context

---
+++ composer/2.1.3/docker-entrypoint.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+check_command() {
+  for cmd in \
+    "about" \
+    "archive" \
+    "browse" \
+    "cc" \
+    "check-platform-reqs" \
+    "clear-cache" \
+    "clearcache" \
+    "config" \
+    "create-project" \
+    "depends" \
+    "diagnose" \
+    "dump-autoload" \
+    "dumpautoload" \
+    "exec" \
+    "fund" \
+    "global" \
+    "help" \
+    "home" \
+    "i" \
+    "info" \
+    "init" \
+    "install" \
+    "licenses" \
+    "list" \
+    "outdated" \
+    "prohibits" \
+    "reinstall" \
+    "remove" \
+    "require" \
+    "run" \
+    "run-script" \
+    "search" \
+    "self-update" \
+    "selfupdate" \
+    "show" \
+    "status" \
+    "suggests" \
+    "u" \
+    "update" \
+    "upgrade" \
+    "validate" \
+    "why" \
+    "why-not"
+  do
+    if [ -z "${cmd#"$1"}" ]; then
+      return 0
+    fi
+  done
+
+  return 1
+}
+
+function main() {
+	# check if the first argument passed in looks like a flag
+	if [ "$(printf %c "$1")" = '-' ]; then
+	  set -- composer "$@"
+	# check if the first argument passed in matches a known command
+	elif check_command "$1"; then
+	  set -- composer "$@"
+	fi
+
+	exec "$@"
+}
+
+main "$@"
